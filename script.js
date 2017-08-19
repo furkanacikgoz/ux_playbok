9f4f7154f85af47d5486604a7f96ebf58a139a67
Furkan <acikgoz.furkan@gmail.com>
2017-08-19 23:32:42 +0300

    typed and content
    -content updated
    -js typed added

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+
+
+
 $( document ).ready(function() {
 	
+	var imported = document.createElement('script');
+	imported.src = 'typed.min.js';
+	document.head.appendChild(imported);	
+
 	$("#section-1").show();
 	
 	//When select a site type
-	$( "#site-type" ).change(function() {
+	$( "#question-1" ).change(function() {
 		console.log($( "option:selected", this ).attr("qID"));
 		var qID = $( "option:selected", this ).attr("qID");
 		console.log(qID);
@@ -15,12 +22,22 @@ $( document ).ready(function() {
 	});
 
 
-	$( "#device-type" ).change(function() {
+	$( "#question-2" ).change(function() {
 		
 		var qID = $( "option:selected", this ).attr("qID");
 		$("#section-2 ul").hide();
 		$("#section-2 ul#" + qID).show("fast");
+		$("#section-3").show("slow");
 	});
+
+	$( "#question-3" ).change(function() {
+		
+		var qID = $( "option:selected", this ).attr("qID");
+		$("#section-3 ul").hide();
+		$("#section-3 ul#" + qID).show("fast");
+		
+	});
+
 
 	//paramaters -
 	/*
@@ -28,4 +45,33 @@ $( document ).ready(function() {
 	section-id
 
 	*/
+
+	//method title added
+	$(".list-items ul").prepend('<li class="methods-title">Önerilen Metodlar</li>')
+
+	//tabs style
+	$( ".tabs li a" ).click(function() {
+		console.log($(" ul li a", this));
+
+		$("ul", this).removeClass("selected-tab");
+		$(this).addClass("selected-tab");
+	});
+
+
+	//typed func.
+	var typed = new Typed('.title-text', {
+	strings: ["kullanıcılarınız ne söylüyor?", "kullanıcılarınız nasıl davranıyor?", "ürününüz kullanılabilir mi?", "kullanıcılar bu ürüne parar verir mi?", "kırmızı buton mu? mavi mi?", "müşterilerinizi anlayın", "kullanıcı deneyiminizi iyileştirin!"],
+        typeSpeed: 40,
+        backSpeed: 10,
+        startDelay: 1000,
+        shuffle: true,
+        backDelay: 5000,
+        loop: true,
+        cursorChar: ''
+	});
+	
+
+
+
+
 });
